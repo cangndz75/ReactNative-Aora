39c73d8554e67728e9b2795adf2b48af0aba875f
Can Gündüz <cangunduz0001@gmail.com>
2024-05-09 00:55:31 +0300

    trending videos v1.0.2

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,18 +1,28 @@
-import { FlatList, Image, Text, View } from "react-native";
-import React from "react";
+import { Alert, FlatList, Image, RefreshControl, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
 import EmptyState from "../../components/EmptyState";
+import {getAllPosts} from "../../lib/appwrite";
+import useAppwrite from "../../lib/useAppwrite";
 const Home = () => {
+  const {data:posts, refetch} = useAppwrite(getAllPosts);
+  const [refreshing, setRefreshing] = useState(false)
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  }
+  console.log(posts);
   return (
-    <SafeAreaView className="bg-primary">
+    <SafeAreaView className="bg-primary h-full">
       <FlatList
-        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        data={posts}
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
-          <Text className="text-3xl text-white">{item.id}</Text>
+          <Text className="text-3xl text-white">{item.title}</Text>
         )}
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
@@ -41,6 +51,7 @@ const Home = () => {
         ListEmptyComponent={()=> (
           <EmptyState title="No Videos Found!" subtitle="Be the first one to upload a video."/>
         )}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}
       />
     </SafeAreaView>
   );
