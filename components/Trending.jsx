8481b421cef0c2bb369dfe3ecf73f4f9b8d351b9
Can Gündüz <cangunduz0001@gmail.com>
2024-05-10 18:36:35 +0300

    trending videos v1.1

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -1,13 +1,71 @@
-import { View, Text, FlatList } from "react-native";
-import React from "react";
+import {
+  Image,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+import * as Animatable from "react-native-animatable";
+import React, { act, useState } from "react";
+import { icons } from "../constants";
 
+const zoomIn = {
+  0: {
+    scale: 0.9,
+  },
+  1: {
+    scale: 1,
+  },
+};
+
+const zoomOut = {
+  0: {
+    scale: 1,
+  },
+  1: {
+    scale: 0.9,
+  },
+};
+
+const TrendingItem = ({ activeItem, item }) => {
+  const [play, setPlay] = useState(false);
+  return (
+    <Animatable.View
+      className="mr-5"
+      animation={activeItem.$id === item.$id ? zoomIn : zoomOut}
+      duration={500}
+    >
+      {play ? (
+        <Text className="text-white">Playing</Text>
+      ) : (
+        <TouchableOpacity
+          className="relative justify-center items-center"
+          activeOpacity={0.7}
+          onPress={() => setPlay(true)}
+        >
+          <ImageBackground
+            source={{ uri: item.thumbnail }}
+            className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40"
+            resizeMode="cover"
+          />
+          <Image
+            source={icons.play}
+            className="w-12 h-12 absolute"
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      )}
+    </Animatable.View>
+  );
+};
 const Trending = ({ posts }) => {
+  const [activeItem, setActiveItem] = useState(posts[1]);
   return (
     <FlatList
       data={posts}
       keyExtractor={(item) => item.$id}
       renderItem={({ item }) => (
-        <Text className="text-3xl text-white">{item.id}</Text>
+        <TrendingItem activeItem={activeItem} item={item} />
       )}
       horizontal
     />
